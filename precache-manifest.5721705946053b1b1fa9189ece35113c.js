80b82e77b47623193e11af73139d632c74ca22a9
Tsimafei Kavalevich <Tsimafei.Kavalevich@itechart-group.com>
2020-04-27 18:39:03 +0300

    Updates

---
+++ precache-manifest.5721705946053b1b1fa9189ece35113c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9efd0c15749217508b40",
+    "url": "/spiral-app/static/js/main.d9a437e9.chunk.js"
+  },
+  {
+    "revision": "ffdfbd9e05e3507cae5b",
+    "url": "/spiral-app/static/js/runtime~main.b65ae8f0.js"
+  },
+  {
+    "revision": "af8709438a01e060ab29",
+    "url": "/spiral-app/static/js/2.647d2793.chunk.js"
+  },
+  {
+    "revision": "45cfb1d7fb022bb0d03385f1521f0212",
+    "url": "/spiral-app/index.html"
+  }
+];
